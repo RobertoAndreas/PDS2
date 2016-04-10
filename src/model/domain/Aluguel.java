e1f7ce3569e8d2449135077f1a8321f999bc6e39
RobertoAndreas <roberto_andreas@hotmail.com>
2016-04-10 15:31:53 -0300

    .

---
+++ src/model/domain/Aluguel.java
@@ -0,0 +1,88 @@
+package model.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_ALUGUEL")
+public class Aluguel implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4291797618210467834L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="NR_ALUGUEL")
+	private Integer numero;
+	
+	@ManyToOne
+	@JoinColumn(name="CD_CLIENTE",referencedColumnName="CD_CLIENTE")
+	private Cliente cliente;
+	
+	@Column(name="TP_ALUGUEL")
+	private String tipo;
+
+	public Aluguel() {
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((numero == null) ? 0 : numero.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluguel other = (Aluguel) obj;
+		if (numero == null) {
+			if (other.numero != null)
+				return false;
+		} else if (!numero.equals(other.numero))
+			return false;
+		return true;
+	}
+	
+
+}
